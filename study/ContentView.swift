b2012dd9b8d40ce16de7fada76e36cd76489861f
huangjiang <huangjiang@codemao.cn>
2020-11-23 18:27:35 +0800

    first commit

--- study/ContentView.swift
+++ study/ContentView.swift
@@ -10,12 +10,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        return ZStack(content: {
+            RoundedRectangle(cornerRadius: 10.0)
+            Text("Hello there, World!")
+        })
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        return HStack{
+            
+            ForEach(0..<4, content: {index in
+                CardeView()
+            })
+            
+        }
+        
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
     }
+}
+
+struct CardeView: View {
+    var body: some View {
+        ZStack{
+            
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("👻")
+        }
+    }
+}
+
+func s() -> String {
+    let s = "s"
+    return s
 }
