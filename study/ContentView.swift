b6ff415284606fa64341eb3bbc965908e8750d57
huangjiang <huangjiang@codemao.cn>
2020-11-23 16:01:32 +0800

    Initial Commit

---
+++ study/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  study
+//
+//  Created by hvah on 2020/11/23.
+//  Copyright © 2020 hvah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
